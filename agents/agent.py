3d3107af07722ef5655d75dcff19f9bf77fca918
Cristian Abrante <cristian@abrante.me>
2020-12-04 20:22:50 +0100

    Bugs fixed in agent and main

--- agents/agent.py
+++ agents/agent.py
@@ -20,8 +20,7 @@ def preprocess_image(observation):
                 preprocessed_img[i, j] = 0
             else:
                 preprocessed_img[i, j] = 1
-
-    return preprocessed_img.astype(np.float).ravel()
+    return torch.from_numpy(preprocessed_img.astype(np.float32).ravel()).unsqueeze(0)
 
 
 def discount_rewards(r, gamma):
@@ -33,32 +32,38 @@ def discount_rewards(r, gamma):
     return discounted_r
 
 
-class Policy(nn.Module):
-    def __init__(self, state_space, observation_space):
-        super(Policy, self).__init__()
+def subtract_observations(current_obs, prev_obs=None):
+    if prev_obs is None:
+        return preprocess_image(current_obs)
+    else:
+        return preprocess_image(current_obs) - preprocess_image(prev_obs)
 
+
+class Policy(nn.Module):
+    def __init__(self, observation_space, action_space):
+        super(Policy, self).__init__()
         self.eps_clip = 0.1
 
         # TODO: Change structure with CNN.
         # pixels_size = image_shape[0] * image_shape[1] * image_shape[2]
         # Neural network is defined as a sequential structure.
         self.layers = nn.Sequential(
-            nn.Linear(state_space, 512),
+            nn.Linear(observation_space, 512),
             nn.ReLU(),
-            nn.Linear(512, observation_space),
+            nn.Linear(512, action_space),
         )
 
-    def forward(self, observation, deterministic=False):
+    def forward(self, observation):
         """
         In this function the observation is processed in the neural network
         """
-        with torch.no_grad():
-            logits = self.layers(observation)
-            return logits
+        # print(observation)
+        logits = self.layers(observation)
+        return logits
 
 
 class Agent(object):
-    def __init__(self, policy, env, player_id=1):
+    def __init__(self, policy, env):
         self.env = env
         self.eps_clip = 0.1
         self.name = "Simplest AI"
@@ -99,50 +104,51 @@ class Agent(object):
         """
         return self.name
 
-    def pre_process(self, x, prev_x):
-        return preprocess_image(x) - preprocess_image(prev_x)
-
     def get_action(self, observation, deterministic=True):
+        """
+        Get action from observation, observation is already in desired format
+        """
         # Observation is computed.
-        processed_observation = \
-            torch.from_numpy(preprocess_image(observation)).to(self.train_device)
+        processed_observation = observation.to(self.train_device)
 
         # The output of the network is computed here
         logits = self.policy.forward(processed_observation)
 
         # If deterministic take the maximum logit, else, sample from categorical distribution.
         if deterministic:
-            action = int(torch.argmax(logits[0]).detach().cpu().numpy()[0])
-            action_prob = 1.0
+            action = torch.argmax(logits[0])
+            action_prob = torch.Tensor(1.0)
         else:
             distribution = Categorical(logits=logits)
-            action = int(distribution.sample().cpu().numpy()[0])
-            print(action)
-            action_prob = float(distribution.probs[0, action].detach().cpu().numpy()[0])
+            action = distribution.sample()
+            action_prob = distribution.log_prob(action)
 
         return action, action_prob
 
-    # TODO: Implement the update of the policy.
+    # Implement the update of the policy.
     def update_policy(self, episode_number):
-        observations = torch.stack(self.observations, dim=0).to(self.train_device).squeeze(-1)
+        observations = torch.stack(self.observations, dim=0).to(self.train_device).squeeze()
+        actions = torch.stack(self.actions, dim=0).to(self.train_device).squeeze(-1)
         action_probs = torch.stack(self.action_probs, dim=0).to(self.train_device).squeeze(-1)
         rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
 
         # Internal state is reset.
-        self.observations, self.action_probs, self.rewards = [], [], []
+        self.observations, self.actions, self.action_probs, self.rewards = [], [], [], []
 
         # Compute discounted rewards
         discounted_rewards = discount_rewards(rewards, self.gamma)
 
-        # Compute the optimization based on the loss
         self.optimizer.zero_grad()
+
+        # Compute the optimization based on the loss
         # Application of PPO
-        vs = np.array([[1., 0.], [0., 1.]])
-        ts = torch.FloatTensor(vs[action_probs.cpu().numpy()])
+        vs = np.array([[1., 0., 0], [0., 1., 0.], [0., 0., 1.]])
+        ts = torch.FloatTensor(vs[actions.cpu().numpy()])
 
         # Update of the network params using loss.
-        logits = self.policy(action_probs)
+        logits = self.policy.forward(observations)
         r = torch.sum(F.softmax(logits, dim=1) * ts, dim=1) / action_probs
+
         loss1 = r * discounted_rewards
         loss2 = torch.clamp(r, 1 - self.eps_clip, 1 + self.eps_clip) * discounted_rewards
         loss = -torch.min(loss1, loss2)
--- agents/main.py
+++ agents/main.py
@@ -1,14 +1,21 @@
 import gym
-import torch
 import wimblepong
-import matplotlib.pyplot as plt
 import numpy as np
+from agent import Agent, Policy, subtract_observations
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
 
 env = gym.make('WimblepongVisualSimpleAI-v0')
-train_episodes = 5
+train_episodes = 50
 
-# implement this part in the agent
-# opt = torch.optim.Adam(policy.parameters(), lr=1e-3)
+# TODO: Change when using a convolutional layer
+observation_space_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
+action_space_dim = 3
+
+# Instantiate the agent and the policy.
+policy = Policy(observation_space_dim, action_space_dim)
+agent = Agent(policy, env)
 
 x_train, y_train, rewards = [], [], []
 reward_sum = 0
@@ -17,6 +24,8 @@ episode_nb = 0
 # Arrays to keep track of rewards
 reward_history, timestep_history = [], []
 average_reward_history = []
+win_rate = []
+average_win_rate = []
 
 # The training loop is run per episode
 for episode in range(train_episodes):
@@ -24,19 +33,19 @@ for episode in range(train_episodes):
     done = False
 
     # the environment is reset each episode.
-    observation = env.reset()
-
+    observation, previous_observation = env.reset(), None
     while not done:
-        # TODO: Get action from the agent agent.get_action(observation)
-        # TODO: Compute the difference between current and past action.
-        action, action_prob = env.MOVE_UP, 0.0
+        # Compute training observation with the difference of previous and current observation.
+        training_observation = subtract_observations(observation, previous_observation)
+
+        action, action_prob = agent.get_action(training_observation, deterministic=False)
         previous_observation = observation
 
         # Action is performed in the environment.
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, info = env.step(action.detach().cpu().numpy()[0])
 
-        # TODO: Store action's outcome (so that the agent can improve its policy)
-        # agent.store_outcome(previous_observation, action_prob, action, reward)
+        # Store action's outcome (so that the agent can improve its policy)
+        agent.store_outcome(training_observation, action, action_prob, reward)
 
         # Store rewards
         reward_sum += reward
@@ -44,14 +53,24 @@ for episode in range(train_episodes):
 
     print(f"Episode {episode} finished | total reward -> {reward_sum}")
 
+    # TODO: implement win counter.
+
     # Keeping records for future plots
     reward_history.append(reward_sum)
     timestep_history.append(timesteps)
     avg = np.mean(reward_history[-100:] if episode > 100 else reward_history)
     average_reward_history.append(avg)
 
-    # TODO: agent should update the policy.
-    # agent.episode_finished(episode)
+    agent.update_policy(episode)
+
+plt.plot(reward_history)
+plt.plot(average_reward_history)
+plt.legend(["Reward", "100-episode reward average"])
+plt.title("Reward history")
+plt.savefig("plots/reward-history.png")
+plt.show()
+
+torch.save(agent.policy.state_dict(), "model_%s_%d.mdl")
 
 if __name__ == "__main__":
     print("end")
